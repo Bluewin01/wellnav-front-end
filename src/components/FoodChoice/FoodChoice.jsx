428ffda9f481b9e7a0885808938a5f21bf0718f8
Jesen Yeoko <jyeoko@gmail.com>
2024-05-30 17:57:52 +0700

    Remove 'Favorite' on Food choice

--- src/components/FoodChoice/FoodChoice.jsx
+++ src/components/FoodChoice/FoodChoice.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./FoodChoice.css";
-import exampleImage from "../../assets/images/Scrambled Egg with Tofu.webp";
+import exampleImage from "../../Assets/Salad.png";
 
 const FoodChoice = ({ imageUrl, foodName, foodDetails }) => {
   const [isFavorite, setIsFavorite] = useState(false);
@@ -20,17 +20,6 @@ const FoodChoice = ({ imageUrl, foodName, foodDetails }) => {
       </div>
       <div className="button-group">
         <button className="add-button">Add</button>
-        <div className="favorite">
-          <button
-            className={`favorite-button ${isFavorite ? "active" : ""}`}
-            onClick={toggleFavorite}
-          >
-            <i
-              className={`bi ${isFavorite ? "bi-heart-fill" : "bi-heart"}`}
-            ></i>
-          </button>
-          <span className="favorite-count">12</span>
-        </div>
       </div>
     </div>
   );
