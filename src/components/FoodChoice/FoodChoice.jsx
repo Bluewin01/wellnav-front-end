8636e389e8f6ebd48908ad2293e36758f9f10b7b
Jesen Yeoko <jyeoko@gmail.com>
2024-06-06 14:48:49 +0700

    Add Redux logic to FoodChoice

--- src/components/FoodChoice/FoodChoice.jsx
+++ src/components/FoodChoice/FoodChoice.jsx
@@ -1,28 +1,37 @@
-import React, { useState, useEffect } from "react";
+// src/components/FoodChoice/FoodChoice.js
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  incrementCounter,
+  decrementCounter,
+  setSelectedItems,
+} from "../../redux/actions";
 import "./FoodChoice.css";
 import exampleImage from "../../Assets/Salad.png";
 
-const FoodChoice = ({ counter, onCounterChange }) => {
+const FoodChoice = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const selectedItems = useSelector((state) => state.selectedItems);
+
   const foodChoices = [
-    { id: 1, name: "Nama Makanan", detail: "Detail Makanan" },
-    { id: 2, name: "Nama Makanan", detail: "Detail Makanan" },
-    { id: 3, name: "Nama Makanan", detail: "Detail Makanan" },
-    { id: 4, name: "Nama Makanan", detail: "Detail Makanan" },
-    { id: 5, name: "Nama Makanan", detail: "Detail Makanan" },
-    { id: 6, name: "Nama Makanan", detail: "Detail Makanan" },
-    { id: 7, name: "Nama Makanan", detail: "Detail Makanan" },
+    { id: 1, name: "Nama Makanan 1", detail: "Detail Makanan 1" },
+    { id: 2, name: "Nama Makanan 2", detail: "Detail Makanan 2" },
+    { id: 3, name: "Nama Makanan 3", detail: "Detail Makanan 3" },
+    { id: 4, name: "Nama Makanan 4", detail: "Detail Makanan 4" },
+    { id: 5, name: "Nama Makanan 5", detail: "Detail Makanan 5" },
+    { id: 6, name: "Nama Makanan 6", detail: "Detail Makanan 6" },
+    { id: 7, name: "Nama Makanan 7", detail: "Detail Makanan 7" },
   ];
 
-  const [selectedItems, setSelectedItems] = useState({});
-
   const handleButtonClick = (id) => {
-    setSelectedItems((prevSelectedItems) => {
-      const isSelected = prevSelectedItems[id];
-      const newSelectedItems = { ...prevSelectedItems, [id]: !isSelected };
-      const newCounter = isSelected ? counter - 1 : counter + 1;
-      onCounterChange(newCounter);
-      return newSelectedItems;
-    });
+    const isSelected = selectedItems.some((item) => item.id === id);
+    const newSelectedItems = isSelected
+      ? selectedItems.filter((item) => item.id !== id)
+      : [...selectedItems, foodChoices.find((item) => item.id === id)];
+
+    dispatch(setSelectedItems(newSelectedItems));
+    dispatch(isSelected ? decrementCounter() : incrementCounter());
   };
 
   return (
@@ -30,7 +39,6 @@ const FoodChoice = ({ counter, onCounterChange }) => {
       {foodChoices.map((item) => (
         <div key={item.id} className="food-choice-container">
           <img src={exampleImage} alt={item.name} className="food-image" />
-
           <div className="food-details">
             <h3>{item.name}</h3>
             <p>{item.detail}</p>
@@ -38,7 +46,11 @@ const FoodChoice = ({ counter, onCounterChange }) => {
           <div className="button-group">
             <button
               className={`add-button ${
-                selectedItems[item.id] ? "selected" : ""
+                selectedItems.some(
+                  (selectedItem) => selectedItem.id === item.id
+                )
+                  ? "selected"
+                  : ""
               }`}
               onClick={() => handleButtonClick(item.id)}
             >
