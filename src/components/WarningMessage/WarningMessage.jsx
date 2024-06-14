528e7473baf7c8415414d7521d98a99c08700ac0
Jesen Yeoko <jyeoko@gmail.com>
2024-06-14 16:03:42 +0700

    add Subtract Image to warning message

--- src/components/WarningMessage/WarningMessage.jsx
+++ src/components/WarningMessage/WarningMessage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "../WarningMessage/WarningMessage.css";
+import subtractImage from "../../Assets/Subtract.png";
 
 const WarningMessage = () => {
   return (
     <div className="warning-message-container">
-      <h1>Perhatian,</h1>
+      <h1>
+        <img
+          src={subtractImage}
+          alt="subtractImage"
+          className="subtract-image"
+        ></img>
+        Perhatian,
+      </h1>
       <h1>
         Makanan yang anda makan tidak baik untuk kondisi kesehatan saat ini
       </h1>
